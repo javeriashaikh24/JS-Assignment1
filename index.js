f120580af205dee61e1cf9ac5156033b6dd600c5
Javeria shaikh <158833259+javeriashaikh24@users.noreply.github.com>
2024-05-11 13:28:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+ //  alert Sec //
+ alert("Error! please enter a valid password");
+ alert("welcome to js land\n happy coding ");
+ alert("welcome to js land...");
+ alert("happy coding!");
+ alert("Hello i can run JS through my web browser's console");
+ 
+ 
+     //   Vriable for string  //
+ var userName = ("username");
+ var myName = ("javeria");
+ var message = ("Hello World");
+ console.log(userName);
+ console.log(myName);
+ console.log(message);
+
+
+  myName = "John Doe";
+ alert(myName);
+
+age = ("15 years old");
+alert (age);
+message = "Hello World";
+alert(message);
+
+ email =("my email password is javeriashaikh@gmail.com");
+ alert(email);
+
+ var book = "A smarter way to learn JavaScript";
+alert(`I am trying to learn from the Book ${book}`);
+
+var myDesign = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(myDesign);
+
+
+ var age = 19;
+alert(`I am ${age} years old`);
+
+var studentName =("javeria");
+     studentAge =("20");
+     studentgender =("Female");
+     studentCountry =("pakistan");
+alert ( "studentName" /+ "studentAge" + "studentgender" + "studentCountry");
+
+document.write("Variable names can only contain letters, numbers, underscores, and dollar signs. For example: $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore, or dollar sign. For example: $name, _name, or name<br>");
+document.write("Variable names are case-sensitive.<br>");
+document.write("Variable names should not be JavaScript keywords.<br>");
+
